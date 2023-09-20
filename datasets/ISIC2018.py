d312e45f4f46056caad1fdca40f8fb5a08931ae2
Autumniese <73429753+Autumniese@users.noreply.github.com>
2023-09-20 17:47:40 +0800

    Update ISIC2018.py

--- datasets/ISIC2018.py
+++ datasets/ISIC2018.py
@@ -119,8 +119,8 @@ class ISIC2018(Dataset):
 
         # read data
         try:
-            # img_data = io.read_image(img_path).float()
-            img_data = Image.open(img_path).convert('RGB').float()
+            img_data = io.read_image(img_path).float()
+            # img_data = Image.open(img_path).convert('RGB').float()
         except Exception as e:
             print("Error when trying to read data file:", e)
             return None
