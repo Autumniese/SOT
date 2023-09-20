bece360b5cfe74570c1e8144fce979cacb849a57
Autumniese <73429753+Autumniese@users.noreply.github.com>
2023-09-20 18:09:35 +0800

    Update ISIC2018.py

--- datasets/ISIC2018.py
+++ datasets/ISIC2018.py
@@ -179,7 +179,7 @@ def get_transform(img_size: int, split_name: str):
 
     else:
         return transforms.Compose([
-            transforms.Resize(size=(img_size, img_size), antialias=True),
+            transforms.Resize((img_size, img_size)),
             transforms.ToTensor(),
             normalize
         ])
