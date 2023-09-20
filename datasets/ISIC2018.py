ab5ca902470c51b1b408b6e9ca2197c056881933
Autumniese <73429753+Autumniese@users.noreply.github.com>
2023-09-20 22:32:45 +0800

    Update ISIC2018.py

--- datasets/ISIC2018.py
+++ datasets/ISIC2018.py
@@ -35,6 +35,8 @@ class ISIC2018(Dataset):
 
         if setname == "train":
             set_name = "Training"
+        else:
+            pass
 
         # Fetch CSV
         self.csv_path = os.path.join(
