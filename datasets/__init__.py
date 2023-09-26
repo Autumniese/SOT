7c9913027156e8e3dc8f2446271f7fafe8f7d5fb
Autumniese <73429753+Autumniese@users.noreply.github.com>
2023-09-26 23:18:00 +0800

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,3 +2,4 @@ from datasets.cifar import CIFAR
 from datasets.cub import CUB
 from datasets.mini_imagenet import MiniImageNet
 from datasets.ISIC2018 import ISIC2018
+from datasets.BreakHis import BreakHis
