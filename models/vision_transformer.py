ae89a183005633613bf1fbfa24013712c9eb1e24
CMeng33 <chewcheemeng4954@gmail.com>
2024-02-04 23:51:32 +0800

    Fit Rotation Loss to ViT backbone & Add Num Class to ViT

--- models/vision_transformer.py
+++ models/vision_transformer.py
@@ -498,7 +498,6 @@ class MedViT(nn.Module):
         self.norm = nn.BatchNorm2d(output_channel, eps=NORM_EPS)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.relu = nn.ReLU()
         # final fc layer
         self.proj_head = nn.Sequential(
             nn.Linear(output_channel, num_classes),
@@ -539,23 +538,134 @@ class MedViT(nn.Module):
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.proj_head(x)
-        x = self.relu(x)
         return x
 
+class SSL_MedViT(nn.Module):
+    def __init__(self, stem_chs, depths, path_dropout, attn_drop=0, drop=0, num_classes=-1,
+                 strides=[1, 2, 2, 2], sr_ratios=[8, 4, 2, 1], head_dim=32, mix_block_ratio=0.75,
+                 use_checkpoint=False):
+        super(SSL_MedViT, self).__init__()
+        self.use_checkpoint = use_checkpoint
+
+        self.stage_out_channels = [[96] * (depths[0]),
+                                   [192] * (depths[1] - 1) + [256],
+                                   [384, 384, 384, 384, 512] * (depths[2] // 5),
+                                   [768] * (depths[3] - 1) + [1024]]
+
+        # Next Hybrid Strategy
+        self.stage_block_types = [[ECB] * depths[0],
+                                  [ECB] * (depths[1] - 1) + [LTB],
+                                  [ECB, ECB, ECB, ECB, LTB] * (depths[2] // 5),
+                                  [ECB] * (depths[3] - 1) + [LTB]]
+
+        self.stem = nn.Sequential(
+            ConvBNReLU(3, stem_chs[0], kernel_size=3, stride=2),
+            ConvBNReLU(stem_chs[0], stem_chs[1], kernel_size=3, stride=1),
+            ConvBNReLU(stem_chs[1], stem_chs[2], kernel_size=3, stride=1),
+            ConvBNReLU(stem_chs[2], stem_chs[2], kernel_size=3, stride=2),
+        )
+        input_channel = stem_chs[-1]
+        features = []
+        idx = 0
+        dpr = [x.item() for x in torch.linspace(0, path_dropout, sum(depths))]  # stochastic depth decay rule
+        for stage_id in range(len(depths)):
+            numrepeat = depths[stage_id]
+            output_channels = self.stage_out_channels[stage_id]
+            block_types = self.stage_block_types[stage_id]
+            for block_id in range(numrepeat):
+                if strides[stage_id] == 2 and block_id == 0:
+                    stride = 2
+                else:
+                    stride = 1
+                output_channel = output_channels[block_id]
+                block_type = block_types[block_id]
+                if block_type is ECB:
+                    layer = ECB(input_channel, output_channel, stride=stride, path_dropout=dpr[idx + block_id],
+                                drop=drop, head_dim=head_dim)
+                    features.append(layer)
+                elif block_type is LTB:
+                    layer = LTB(input_channel, output_channel, path_dropout=dpr[idx + block_id], stride=stride,
+                                sr_ratio=sr_ratios[stage_id], head_dim=head_dim, mix_block_ratio=mix_block_ratio,
+                                attn_drop=attn_drop, drop=drop)
+                    features.append(layer)
+                input_channel = output_channel
+            idx += numrepeat
+        self.features = nn.Sequential(*features)
+
+        self.num_classes = num_classes
+
+        self.norm = nn.BatchNorm2d(output_channel, eps=NORM_EPS)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.relu = nn.ReLU()
+        self.rot_classifier = nn.Linear(self.num_classes, 4)
+        # final fc layer
+        self.proj_head = nn.Sequential(
+            nn.Linear(output_channel, num_classes),
+        )
+
+        self.stage_out_idx = [sum(depths[:idx + 1]) - 1 for idx in range(len(depths))]
+        print('initialize_weights...')
+        self._initialize_weights()
+
+    def merge_bn(self):
+        self.eval()
+        for idx, module in self.named_modules():
+            if isinstance(module, ECB) or isinstance(module, LTB):
+                module.merge_bn()
+
+    def _initialize_weights(self):
+        for n, m in self.named_modules():
+            if isinstance(m, (nn.BatchNorm2d, nn.GroupNorm, nn.LayerNorm, nn.BatchNorm1d)):
+                nn.init.constant_(m.weight, 1.0)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                trunc_normal_(m.weight, std=.02)
+                if hasattr(m, 'bias') and m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Conv2d):
+                trunc_normal_(m.weight, std=.02)
+                if hasattr(m, 'bias') and m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, x, is_feat=False, rot=False):
+        x = self.stem(x)
+        for idx, layer in enumerate(self.features):
+            if self.use_checkpoint:
+                x = checkpoint.checkpoint(layer, x)
+            else:
+                x = layer(x)
+        x = self.norm(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        # x = self.proj_head(x)
+        
+        feat = x
+        xx = self.classifier(x)
+        xx = self.relu (xx)
+
+        if(rot):
+            xy = self.rot_classifier(xx)
+            return [_, _, _, _, feat], (xx, xy)
+
+        if is_feat:
+            return [_, _, _, _, feat], xx
+        else:
+            return xx
 
 @register_model
 def MedViT_small(pretrained=False, pretrained_cfg=None, **kwargs):
-    model = MedViT(stem_chs=[64, 32, 64], depths=[3, 4, 10, 3], path_dropout=0.1, **kwargs)
+    model = SSL_MedViT(stem_chs=[64, 32, 64], depths=[3, 4, 10, 3], path_dropout=0.1, **kwargs)
     return model
 
 
 @register_model
 def MedViT_base(pretrained=False, pretrained_cfg=None, **kwargs):
-    model = MedViT(stem_chs=[64, 32, 64], depths=[3, 4, 20, 3], path_dropout=0.2, **kwargs)
+    model = SSL_MedViT(stem_chs=[64, 32, 64], depths=[3, 4, 20, 3], path_dropout=0.2, **kwargs)
     return model
 
 
 @register_model
 def MedViT_large(pretrained=False, pretrained_cfg=None, **kwargs):
-    model = MedViT(stem_chs=[64, 32, 64], depths=[3, 4, 30, 3], path_dropout=0.2, **kwargs)
+    model = SSL_MedViT(stem_chs=[64, 32, 64], depths=[3, 4, 30, 3], path_dropout=0.2, **kwargs)
     return model
